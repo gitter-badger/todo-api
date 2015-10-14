9e9c0388fe45dbcea985e32b87e01667b5e268a9
Brian Dawson <brianvdawson@gmail.com>
2015-10-14 00:01:36 -0700

    Correcting create method

--- src/main/java/com/cloudbees/examples/todo/api/service/HashMapToDoService.java
+++ src/main/java/com/cloudbees/examples/todo/api/service/HashMapToDoService.java
@@ -47,7 +47,7 @@ public class HashMapToDoService implements ToDoService {
 		todo.setCreatedDate(Instant.now());
 		todos.put(todo.getId(), todo);
 
-		return todo;              lkds;lfkasdj
+		return null;
 	}
 
 	@Override
