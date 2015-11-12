11e4146794187026216c966b67efd9d401477168
Brian Dawson <brianvdawson@gmail.com>
2015-11-11 18:13:01 -0800

    Adding example workflow script file

---
+++ perf-pipeline.groovy
@@ -0,0 +1,27 @@
+stage "unit test"
+
+node {
+    git "https://github.com/brianvdawson/todo-api.git"
+    sh "echo unit test app"
+    //sh "mvn package"
+}
+
+checkpoint 'unit testing complete'
+
+stage "prepare deployable"
+
+build "freestyle-test"
+
+checkpoint 'deployable ready'
+
+stage "deployment testing"
+parallel "function test": {
+        node {
+            git "https://github.com/brianvdawson/todo-api.git"
+            sh "echo run functional tests app"
+        }
+    },"performance test": {
+            node {
+                sh "echo performance test app"
+            }
+    }
